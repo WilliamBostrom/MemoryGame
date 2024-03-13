7da28b4fe9fcc9e5e218de0d8034726d92eea80c
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-03-13 21:05:30 +0100

    Spelet klart

---
+++ src/components/SingleCard.jsx
@@ -0,0 +1,34 @@
+import "./SingleCard.css";
+// import PropTypes from "prop-types";
+
+function SingleCard({ card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+
+        <img
+          className="back"
+          src="/img/cover.png"
+          onClick={handleClick}
+          alt="card back"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SingleCard;
+
+/* SingleCard.propTypes = {
+  card: card.array,
+  handleChoice: card,
+  func,
+};
+ */
